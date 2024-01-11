687f749986572ca1d19b4c77b04cfc8c1951dc2d
jessika <jessikafujimura@gmail.com>
2024-01-11 00:01:19 -0300

    create first files

---
+++ app/routes/_index.tsx
@@ -0,0 +1,21 @@
+export default function IndexRoute() {
+    return (
+        <main>
+        <header>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Project</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+        <h1>Hi, I'm Jéssika Mayumi Fujimura</h1>
+        <h3>Backend Developer</h3>
+        </section>
+      </main>
+    );
+  }
+  
---
+++ app/routes/about.tsx
@@ -0,0 +1,12 @@
+export default function aboutRoute(){
+
+return (
+    <section>
+        <h1>
+            About me
+        </h1>
+        <p>I born in July, 1991 in Brazil. I was lived in fourth different cities in Brazil. Nowadays, I am living in São Paulo.</p>
+    </section>
+)
+}
+
